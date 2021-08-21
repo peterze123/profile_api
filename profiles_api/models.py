04c98e658e8c868b9e00502af97fe515a2b1be15
peterze123 <peteztc@gmail.com>
2021-08-20 22:24:14 -0400

    model first commit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class userInfoManager(BaseUserManager):
+
+    def create_usr(self, email, name, password=None):
+        if not email:
+            raise ValueError('Email is a required field')
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_admin(self, email, name, password):
+        user = self.create_usr(email, name, password)
+
+        user.is_superuser = True
+        user.save(using=self._db)\
+
+        return user
+
+class userInfo(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=50, unique=True, primary_key=True)
+    name = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+
+    objects = userInfoManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FOELDS = ['userId']
+
+    def get_id(self):
+        return self.user_id
+
+    def get_name(self):
+        return self.name
+
+    def __repr__(self):
+        return self.email
+

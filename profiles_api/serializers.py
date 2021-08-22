fff5cd622e98140b18b39db2c4c86e5ba6c40bcb
peterze123 <peteztc@gmail.com>
2021-08-22 17:59:01 -0400

    update branch

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
 
+from profiles_api import models
+
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+    class Meta:
+        model = models.userInfo
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password':{
+                'write_only':True,
+                
+            }
+        }
